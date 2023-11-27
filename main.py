9b20634e2cc353f8468ac7683708eb1a1bb6035e
shihabdelwar <shihabdelwar@gmail.com>
2023-11-27 17:31:08 -0500

    updated

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pygame
 import sys
 import random
+import time
 #import your controller
 
 def main():
-    pygame.init()
     #Create an instance on your controller object
     #Call your mainloop
     global timer
     global score
+    global start_time
+
+    start_time = pygame.time.get_ticks()
 
     while timer > 0:
+        clock.tick(60)
         handle_events()
         update_game()
         draw_game()
-        clock.tick(60)
+        # if timer == pygame.USEREVENT: 
+        #     timer -= 1
 
     # Show the final score
     screen.fill(BACKGROUND_COLOR)
@@ -34,7 +39,7 @@ TEXT_COLOR = "black"
 MOLE_RADIUS = 30
 HOLE_SIZE = 60
 TIMER_FONT_SIZE = 36
-GAME_DURATION = 5
+GAME_DURATION = 30
 HOLE_POSITIONS = [(100, 100), (400, 100), (700, 100),
                   (100, 300), (400, 300), (700, 300),
                   (100, 500), (400, 500), (700, 500)]
@@ -44,12 +49,15 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Whack-a-Mole")
 clock = pygame.time.Clock()
+# pygame.time.set_timer(pygame.USEREVENT, GAME_DURATION)
+
 
 # Mole class
 class Mole(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((MOLE_RADIUS * 2, MOLE_RADIUS * 2))
+        self.image = pygame.Surface((MOLE_RADIUS * 2, MOLE_RADIUS * 2), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, pygame.Color(MOLE_COLOR), (MOLE_RADIUS, MOLE_RADIUS), MOLE_RADIUS)
         self.image.fill(MOLE_COLOR)
         self.rect = self.image.get_rect()
         self.rect.center = random.choice(HOLE_POSITIONS)
@@ -69,16 +77,32 @@ def handle_events():
             pygame.quit()
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if mole.rect.collidepoint(event.pos) and not mole.hidden:
+            if mole.rect.colliderect(pygame.Rect(event.pos[0] - MOLE_RADIUS, event.pos[1] - MOLE_RADIUS, MOLE_RADIUS * 2, MOLE_RADIUS * 2)) and not mole.hidden:
                 mole.hide()
                 increase_score()
 
 # Function to update game state
+# def update_game():
+#     global timer
+#     global score
+#     current_time = pygame.time.get_ticks()
+#     if timer > 0:
+#         # Randomly show/hide the mole
+#         if random.random() < 0.02:
+#             if mole.hidden:
+#                 mole.show()
+#             else:
+#                 mole.hide()
+#         if current_time % 1000 == 0:
+#             # Update timer
+#             timer -= 1
 def update_game():
     global timer
     global score
 
-    if timer > 0:
+    current_time = pygame.time.get_ticks()
+
+    if timer > 0 and current_time - start_time >= 1000:
         # Randomly show/hide the mole
         if random.random() < 0.02:
             if mole.hidden:
@@ -87,8 +111,7 @@ def update_game():
                 mole.hide()
 
         # Update timer
-        if pygame.time.get_ticks() % 1000 == 0:
-            timer -= 1
+        timer = GAME_DURATION - ((current_time - start_time) // 1000)
 
 # Function to draw the game
 def draw_game():
