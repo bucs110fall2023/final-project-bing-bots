91db2041988ab69cb50e1968b34ac28305c32fa2
shihabdelwar <shihabdelwar@gmail.com>
2023-11-27 17:46:25 -0500

    updated

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
 import pygame
 import sys
 import random
-import time
-#import your controller
-
-def main():
-    #Create an instance on your controller object
-    #Call your mainloop
-    global timer
-    global score
-    global start_time
-
-    start_time = pygame.time.get_ticks()
-
-    while timer > 0:
-        clock.tick(60)
-        handle_events()
-        update_game()
-        draw_game()
-        # if timer == pygame.USEREVENT: 
-        #     timer -= 1
-
-    # Show the final score
-    screen.fill(BACKGROUND_COLOR)
-    final_score_text = font.render(f'Congratulations, you finished the game with a score of {score}', True, TEXT_COLOR)
-    screen.blit(final_score_text, (WIDTH // 2 - 300, HEIGHT // 2 - 50))
-    pygame.display.flip()
-    pygame.time.wait(3000)  # Display the final score for 3 seconds before quitting
-    
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 # Constants
 WIDTH, HEIGHT = 800, 600
@@ -49,47 +21,55 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Whack-a-Mole")
 clock = pygame.time.Clock()
-# pygame.time.set_timer(pygame.USEREVENT, GAME_DURATION)
-
 
 # Mole class
 class Mole(pygame.sprite.Sprite):
     def __init__(self):
+        """Initialize the Mole sprite."""
         super().__init__()
-        self.image = pygame.Surface((MOLE_RADIUS * 2, MOLE_RADIUS * 2), pygame.SRCALPHA)
-        pygame.draw.circle(self.image, pygame.Color(MOLE_COLOR), (MOLE_RADIUS, MOLE_RADIUS), MOLE_RADIUS)
+        self.image = pygame.Surface((MOLE_RADIUS * 2, MOLE_RADIUS * 2))
         self.image.fill(MOLE_COLOR)
         self.rect = self.image.get_rect()
         self.rect.center = random.choice(HOLE_POSITIONS)
         self.hidden = True
+        self.last_toggle_time = 0
 
     def show(self):
+        """Display the mole."""
         self.hidden = False
 
     def hide(self):
+        """Hide the mole and reset its position."""
         self.hidden = True
         self.rect.center = random.choice(HOLE_POSITIONS)
 
+    def can_toggle(self):
+        """Check if enough time has passed to toggle the mole's visibility."""
+        return pygame.time.get_ticks() - self.last_toggle_time >= 1000
+
 # Function to handle events
 def handle_events():
+    """Handle pygame events, such as quitting the game or clicking the mouse."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if mole.rect.colliderect(pygame.Rect(event.pos[0] - MOLE_RADIUS, event.pos[1] - MOLE_RADIUS, MOLE_RADIUS * 2, MOLE_RADIUS * 2)) and not mole.hidden:
+            if not mole.hidden and mole.rect.collidepoint(event.pos):
                 mole.hide()
                 increase_score()
 
 def update_game():
+    """Update the game state, including mole visibility and the game timer."""
     global timer
     global score
+    mole.update()
 
     current_time = pygame.time.get_ticks()
 
     if timer > 0 and current_time - start_time >= 1000:
         # Randomly show/hide the mole
-        if random.random() < 0.02:
+        if random.random() < 0.02 and mole.can_toggle():
             if mole.hidden:
                 mole.show()
             else:
@@ -100,6 +80,7 @@ def update_game():
 
 # Function to draw the game
 def draw_game():
+    """Draw the game elements on the screen, including holes, mole, timer, and score."""
     screen.fill(BACKGROUND_COLOR)
     # Draw holes
     for pos in HOLE_POSITIONS:
@@ -114,22 +95,45 @@ def draw_game():
 
 # Function to increase the score
 def increase_score():
+    """Increase the player's score."""
     global score
     score += 1
-    
+
+# Main game loop function
+def main_game_loop():
+    """Run the main game loop until the timer reaches zero."""
+    global timer
+    global score
+    global start_time
+
+    start_time = pygame.time.get_ticks()
+
+    while timer > 0:
+        clock.tick(60)
+        handle_events()
+        update_game()
+        draw_game()
+
+    # Show the final score
+    screen.fill(BACKGROUND_COLOR)
+    final_score_text = font.render(f'Congratulations, you finished the game with a score of {score}', True, TEXT_COLOR)
+    screen.blit(final_score_text, (WIDTH // 2 - 300, HEIGHT // 2 - 50))
+    pygame.display.flip()
+    pygame.time.wait(3000)  # Display the final score for 3 seconds before quitting
+
 # Set up timer and score
 font = pygame.font.Font(None, TIMER_FONT_SIZE)
 timer = GAME_DURATION
 score = 0
+start_time = 0
 
 # Create mole
 mole = Mole()
 all_sprites = pygame.sprite.Group(mole)
 
-# Run the game loop
+# Run the main game loop
 if __name__ == '__main__':
-    main()
+    main_game_loop()
 
-# End of the game
-pygame.quit()
-sys.exit()
+    pygame.quit()
+    sys.exit()
