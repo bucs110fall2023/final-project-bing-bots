0fb0b08ba7deb63386709d3f718156cc85b8f6fe
shihabdelwar <shihabdelwar@gmail.com>
2023-11-29 20:43:10 -0500

    updated

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ HOLE_POSITIONS = [(100, 100), (400, 100), (700, 100),
 # Mole class
 class Mole(pygame.sprite.Sprite):
     def __init__(self):
+        """
+        Initializes the Mole object
+        Arguments:
+            self
+        Return:
+            none
+        """
         super().__init__()
         self.image = pygame.Surface((MOLE_RADIUS * 2, MOLE_RADIUS * 2), pygame.SRCALPHA)
         pygame.draw.circle(self.image, MOLE_COLOR, (MOLE_RADIUS, MOLE_RADIUS), MOLE_RADIUS)
@@ -30,15 +37,36 @@ class Mole(pygame.sprite.Sprite):
         self.last_toggle_time = 0
 
     def show(self):
+        """
+        Sets the self.hidden value to False
+        Arguments:
+            self
+        Return:
+            none
+        """
         self.hidden = False
 
     def hide(self):
+        """
+        Sets the self.hidden value to True and sets the center of the rect of the object to a random value from the list of hole positions.
+        Arguments:
+            self
+        Return:
+            none
+        """
         self.hidden = True
         self.rect.center = random.choice(HOLE_POSITIONS)
 
 # WhackAMoleGame class
 class WhackAMoleGame:
     def __init__(self):
+        """
+        Initializes the WhackAMoleGame object
+        Arguments:
+            self
+        Return:
+            none
+        """
         self.mole = Mole()
         self.all_sprites = pygame.sprite.Group(self.mole)
 
@@ -49,10 +77,25 @@ class WhackAMoleGame:
         self.game_started = False
 
     def point_inside_mole(self, point):
+        """
+        Identifies whether or not the point that the player presses down on is inside the radius of the mole using the math module.
+        Arguments:
+            self
+            point
+        Return:
+            a conditional statement that the distance_squared is <= the mole_radius squared
+        """
         distance_squared = (point[0] - self.mole.rect.center[0]) ** 2 + (point[1] - self.mole.rect.center[1]) ** 2
         return distance_squared <= MOLE_RADIUS ** 2
 
     def handle_events(self):
+        """
+        Handles the events of the program
+        Arguments:
+            self
+        Return:
+            none
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -63,9 +106,23 @@ class WhackAMoleGame:
                     self.increase_score()
 
     def increase_score(self):
+        """
+        Increases the score in the score tracker by 1 when called
+        Arguments:
+            self
+        Return:
+            none
+        """
         self.score += 1
 
     def draw_start_screen(self):
+        """
+        Draws the "click to play" starter screen of the program
+        Arguments:
+            self
+        Return:
+            none
+        """
         screen.fill(BACKGROUND_COLOR)
         box_rect = pygame.Rect(WIDTH // 4, HEIGHT // 4, WIDTH // 2, HEIGHT // 2)
         pygame.draw.rect(screen, BOX_COLOR, box_rect)
@@ -75,9 +132,14 @@ class WhackAMoleGame:
         pygame.display.flip()
 
     def show_final_score(self):
+        """
+        Draws the final score page where the final score of the player is shown and the player is congratulated.
+        Arguments:
+            self
+        Return:
+            none
+        """
         screen.fill(BACKGROUND_COLOR)
-        box_rect = pygame.Rect(WIDTH // 4, HEIGHT // 4, WIDTH // 2, HEIGHT // 2)
-        # pygame.draw.rect(screen, BOX_COLOR, box_rect)
         final_score_text = self.font.render(f'Congratulations, you finished the game with a score of {self.score}', True, TEXT_COLOR)
         text_rect = final_score_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
         screen.blit(final_score_text, text_rect)
@@ -85,6 +147,13 @@ class WhackAMoleGame:
         pygame.time.wait(3000)  # Display the final score for 3 seconds before quitting
 
     def run(self):
+        """
+        The main loop function where the actual program is being run.
+        Arguments:
+            self
+        Return:
+            none
+        """
         while True:
             clock.tick(60)
 
@@ -100,7 +169,6 @@ class WhackAMoleGame:
 
             current_time = pygame.time.get_ticks()
             elapsed_time = (current_time - self.start_time) // 1000
-            remaining_time = max(GAME_DURATION - elapsed_time, 0)
 
             if elapsed_time % 1 == 0:
                 if random.random() < 0.02 and self.mole.hidden:
